9d5920a86a620132896beae93dbe75aa6370a9ce
Andrey Sivchikov <sivand@mail.ru>
2020-02-09 19:12:20 +0700

    fix post build script

--- buildroot-2019.02.9/board/olimex/a20_olinuxino/post-build.sh
+++ buildroot-2019.02.9/board/olimex/a20_olinuxino/post-build.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
-cp $BINARIES_DIR/boot.scr $TARGET_DIR/boot/boot.scr
+mkdir -p ${TARGET_DIR}/boot
 
-if [ -e $BINARIES_DIR/script.bin ]; then
+cp ${BINARIES_DIR}/boot.scr ${TARGET_DIR}/boot/boot.scr
+
+if [ -e ${BINARIES_DIR}/script.bin ]; then
 	# mali requires a legacy kernel
-	cp $BINARIES_DIR/script.bin $TARGET_DIR/boot/script.bin
+	cp ${BINARIES_DIR}/script.bin ${TARGET_DIR}/boot/script.bin
 fi
