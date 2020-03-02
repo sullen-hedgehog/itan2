fd90d119de75aa5cde8ebd91a1ffcb215a1ef918
Andrey Sivchikov <sivand@mail.ru>
2020-03-02 21:27:49 +0700

    run X

--- projects/itan2-mmc/board/make-up.sh
+++ projects/itan2-mmc/board/make-up.sh
@@ -2,5 +2,7 @@
 
 rm -f ${TARGET_DIR}/linuxrc
 rm -f ${TARGET_DIR}/etc/init.d/S40xorg
+rm -f ${TARGET_DIR}/etc/init.d/*mdev*
+rm -f ${TARGET_DIR}/usr/share/X11/xorg.conf.d/*
 
 cp -dpfR ../board/skeleton/* ${TARGET_DIR}
